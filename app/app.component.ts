7a8d4aba53508277298ea3678a14b97ccb38e862
April Peng <helloaprilpeng@gmail.com>
2016-10-21 08:09:38 -0700

    set up initial files

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meal-tracker',
+  template: `
+  <h1>Meal Tracker - Meal Hacker</h1>
+  `
+})
+
+export class AppComponent {
+
+}
